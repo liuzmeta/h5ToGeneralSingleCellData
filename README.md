fb1449841533ab1f1a60083b681c21cc63da2317
Zhu Liu <zhuliu@Zhus-MBP-2.attlocal.net>
2022-07-25 09:57:12 -0500

    v1

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # h5ToGeneralSingleCellData
 a script to convert h5 file to general single cell data
+## Usage
+
+e.g. `python convert_h5_to_general.py GSM3489182_Donor_01_filtered_gene_bc_matrices_h5.h5` well create the following content in current directory:
+
+- GSM3489182_Donor_01_filtered_gene_bc_matrices_h5
+  - barcodes.tsv.gz
+  - features.tsv.gz
+  - matrix.mtx.gz
---
+++ convert_h5_to_general.py
@@ -0,0 +1,50 @@
+import gzip
+import os
+import scipy.sparse as sp_sparse
+import scipy.io
+import tables
+import sys
+import io
+
+
+def get_matrix_from_h5(filename):
+    with tables.open_file(filename, 'r') as f:
+        mat_group = f.get_node(f.root, 'GRCh38')
+        barcodes = f.get_node(mat_group, 'barcodes').read()
+        data = getattr(mat_group, 'data').read()
+        indices = getattr(mat_group, 'indices').read()
+        indptr = getattr(mat_group, 'indptr').read()
+        shape = getattr(mat_group, 'shape').read()
+        matrix = sp_sparse.csc_matrix((data, indices, indptr), shape=shape)
+        genes = f.get_node(mat_group, 'genes').read()
+        gene_names = f.get_node(mat_group, 'gene_names').read()
+
+        return (genes, gene_names, barcodes, matrix)
+
+
+def writefile(fname, data):
+    genes, gene_names, barcodes, matrix = data
+    sample = '_'.join(fname.split('.')[:-1])
+    os.mkdir(sample)
+    b_s = io.BytesIO()
+    scipy.io.mmwrite(b_s, matrix)
+    f_out = gzip.open(os.path.join(sample, 'matrix.mtx.gz'), 'wb')
+    f_out.write(b_s.getvalue())
+    f_out.close()
+    write_to_gzip(
+        range(len(genes)), os.path.join(sample, 'features.tsv.gz'), lambda i:
+        genes[i].decode('UTF-8') + '\t' + gene_names[i].decode('UTF-8') + '\n')
+    write_to_gzip(barcodes, os.path.join(sample, 'barcodes.tsv.gz'),
+                  lambda i: i.decode('UTF-8') + '\n')
+
+
+def write_to_gzip(iter, output, line_mod=lambda x: x):
+    f_out = gzip.open(output, 'wb')
+    f_out.writelines([line_mod(i).encode('utf8') for i in iter])
+    f_out.close()
+
+
+if __name__ == "__main__":
+    filename = sys.argv[1]
+    data = get_matrix_from_h5(filename)
+    writefile(filename, data)
